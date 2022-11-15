7f9804ceeb438640f542879b8d1209048623b98a
LiU-SKY <gitamsky1@naver.com>
2022-11-16 02:33:52 +0900

    update crawler, added TODO-list

--- src/crawl.js
+++ src/crawl.js
@@ -1,75 +1,66 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+const id = "20211846";
+const pw = "whgksmf02!";
 
-let MealList = {
-  "date": "",
-  "breakfast": "",
-  "lunch": "",
-  "dinner": "", 
-};
+const crawler = async() => {
+    try{
+        const browser = await puppeteer.launch({headless: false, args:['--window-size=1920,1080']});
+        const page = await browser.newPage();
+        await page.setViewport({
+            width:800,
+            height:600
+        })
+        await page.goto('https://edu.dju.ac.kr/Main.do?cmd=viewHome');
 
-async function crawl(){
-  // 가상 브라우져를 실행, headless: false를 주면 벌어지는 일을 새로운 창을 열어 보여준다(default: true)
-  const browser = await puppeteer.launch({headless: true});
-  const page = await browser.newPage();
-  
-  const ndhs_id = '20211846'; // 추후 로그인 폼에서 각자의 아이디 비밀번호를 입력받게 할 예정
-  const ndhs_pw = 'whgksmf02!';
+        await page.evaluate(({ id,pw }) => {
+            document.querySelector("#id").value = id;
+            document.querySelector("#pw").value = pw;
+            document.querySelector("#loginForm > fieldset > p > a").click();
+            
+        }, { id,pw } )
+        await page.waitForNavigation();
+        //로그인 완료
 
-  // headless: false일때 브라우져 크기 지정해주는 코드
-  await page.setViewport({
-       //width: 1366,
-       //height: 768
-   });
+        //수업목록 불러오기 시작
+        const content = await page.content();
+        const $ = cheerio.load(content);
+        const lists = $("#mainContent > div.center_box > div > ul");
+        
+        var result = lists.text().replaceAll('\t', '').replaceAll(' ', '').split('\n');
+        let res = result.filter((element) => {
+            return element !== undefined && element !== null && element !== '';
+          });
 
-  //페이지로 가라
-  await page.goto('https://edu.dju.ac.kr/');
-  await page.waitFor(500);
-
-  //해당 페이지에 특정 html 태그를 클릭해라
-  await page.click('html > body > div[2] > div[2] > div[2] > div > form > fieldset > ul > li[1] > input');
-  console.log('success1');
-  //아이디랑 비밀번호 란에 값을 넣어라
-  await page.evaluate((id, pw) => {
-  document.querySelector('#id').value = id;
-  document.querySelector('#pw').value = pw;
-  }, ndhs_id, ndhs_pw);
-  //로그인 버튼을 클릭해라
-  await page.click('html > body > div[2] > div[2] > div[2] > div > form > fieldset > p > a');
-
-  //로그인 화면이 전환될 때까지 기다려라, headless: false 일때는 필요 반대로 headless: true일때는 없어야 되는 코드
-  await page.waitForNavigation()
-
-  //로그인 성공 시(화면 전환 성공 시)
-  if(page.url() === 'https://edu.dju.ac.kr/'){
-      //학사 페이지로 가서
-      await page.click('body > div[2] > div[3] > div[2] > div[2] > div > ul > div > div[1] > li[1] > a > span[1]');
-      
-      // 현재 페이지의 html정보를 로드
-      const content = await page.content();
-      const $ = cheerio.load(content);
-      const lists = $("body > div.container-fluid > div:nth-child(6) > div > table > tbody > tr");
-      lists.each((index, list) => {
-          MealList[index] = {
-              date: $(list).find("th").text().replace('\n\t\t\t\t\t\t\t\t',""),
-              breakfast:$(list).find("td:nth-of-type(1)").text(),
-              lunch:$(list).find("td:nth-of-type(2)").text(),
-              dinner:$(list).find("td:nth-of-type(3)").text()
-          }
-          console.log(MealList[index]); 
-
-      })
-  }
-  //로그인 실패시
-  else{
-      console.log('실패');
-      ndhs_id = 'nope';
-      ndhs_pw = 'nope';
-  }
-
-  //브라우저 꺼라
-  await browser.close();     
-};
+        var arr = [];
+        var arrRes = [];
+        for (let i = 0, n = 0; i < res.length; i++, n++){
+            if(n==3){
+                arr = [];
+                n=0;
+            }
+            arr.push(res[i]);
+            if(n==2){
+                arrRes.push(arr);
+            }
+        }
+        
+        console.log(arrRes);
+        //수업목록 불러오기 완료
 
 
-crawl();
+
+        //메인에서 로그아웃
+        await page.evaluate(() => {
+        document.querySelector("#loginForm > fieldset > ul.member > li:nth-child(1) > a").click();
+    })
+        // await page.close();
+        await page.waitForNavigation();
+        await browser.close();
+    }catch(err){
+        console.log('에러발생')
+        console.log(err)
+    }
+}
+
+crawler();
